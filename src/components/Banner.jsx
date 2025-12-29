d8c7088199cd9f923117850ce7436f9a9955cc34
Ruslan <lelyimalikrusik12301@gmail.com>
2025-12-29 22:54:15 +0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import './Banner.css'
 function Banner() {
   return (
     <div className="banner">
-      <img src="/image/Baner.png" alt="CryptoBoss promo" style={{width: '100%', height: 'auto', display: 'block'}} />
+      <img src="/image/Banner.png" alt="CryptoBoss promo" style={{width: '100%', height: 'auto', display: 'block'}} />
     </div>
   )
 }
