bab68bf6e103107fa549fa6c2dbb7320f6e44456
Ruslan <lelyimalikrusik12301@gmail.com>
2026-01-16 17:43:51 +0300

    фы

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -440,9 +440,9 @@ const inventoryPreview = inventoryDrops.slice(0, 4)
 
         {/* ===== WITHDRAW BUTTON ===== */}
       <button
-  className={`withdraw-btn gg-btn-glow ${(!withdrawInfo?.can_withdraw || balance < 0.5) ? 'withdraw-btn--inactive' : ''}`}
+  className={`withdraw-btn gg-btn-glow ${(!withdrawInfo?.can || balance < 0.5) ? 'withdraw-btn--inactive' : ''}`}
   onClick={() => {
-    if (!withdrawInfo?.can_withdraw) {
+    if (!withdrawInfo?.can) {
       showNotification(t('profile.withdrawDepositRequired', { amount: '3' }))
       return
     }
